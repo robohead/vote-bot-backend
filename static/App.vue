4a16950316992641060af04611a7b0ae846b0da2
Dmitriy Tokarev <freemiumhead@gmail.com>
2016-11-08 10:14:46 +0300

    feat(): app work

--- static/App.vue
+++ static/App.vue
@@ -1,31 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="" v-for="vote in votes">
+        <span>sdfsdfsdfsdf</span>
+        <span>{{ vote.author_name }}</span>
+        <span>{{ vote.content }}</span>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to VoteBot page',
+      api: '/api',
+      votes: []
     }
+  },
+  methods: {
+      getAllVotes: function() {
+
+          this.$http.get(this.api).then(function(response) {
+              console.log(response)
+
+              this.votes = response.body.data
+
+          }, function(error) {
+            //   ошибка
+          })
+
+      }
+  },
+  created: function() {
+      this.getAllVotes()
   }
 }
 </script>
--- static/main.js
+++ static/main.js
@@ -1,29 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
+Vue.use(VueMaterial)
+
+Vue.material.theme.register('default', {
+  primary: 'cyan',
+  accent: 'pink'
+})
 
 new Vue({
   el: '#app',
   data: {
-      api: 'https://jsonplaceholder.typicode.com/posts'
-  },
-  methods: {
-      getAllVotes: function() {
 
-          this.$http.get(this.api).then(function(response) {
-
-              console.log(response)
-
-          }, function(error) {
-            //   ошибка
-          })
-
-      }
-  },
-  created: function() {
-      this.getAllVotes()
   },
   render: h => h(App)
 })
